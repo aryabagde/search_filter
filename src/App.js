8689ebb25e97a6c42004b8eed304759faf800095
aryabagde <akruyhau@gmail.com>
2022-09-23 20:44:09 +0530

    Files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Table from './Table';
+import {Users} from './Users';
 
 function App() {
+
+  const [query, setQuery] = useState("");
+
+  // const keys = ["first_name", "last_name", "email"]
+  //console.log(Users[0]["email"]);
+  //const search = (data) => {
+  //   return data.filter((item)) =>
+  //     keys.some((key) => item[key].toLowerCase().includes(query))
+  //    )
+  // }
+
+
+  const search =(data)=>{
+    return data.filter((item) => item.first_name.toLowerCase().includes(query) || item.last_name.toLowerCase().includes(query) || item.email.toLowerCase().includes(query));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+      type="text"
+      placeholder="Search.."
+      className='search'
+      onChange={(e) => setQuery(e.target.value)}      // e.target value is the current value of the placeholder
+      />
+      <Table data={search(Users)}/>
     </div>
   );
 }
